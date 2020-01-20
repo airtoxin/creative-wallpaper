3d82064efb1d0b73f452bcc04e5a69e5c789917f
airtoxin <airtoxin@icloud.com>
2020-01-20 21:28:37 +0900

    Add package

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-// @ts-ignore
-console.log("hello");
+import { Synth } from "tone";
+import { World, Edge, Vec2, Circle } from "planck-js";
+import Renderer, { Runner } from "planck-renderer";
+
+const synth = new Synth().toDestination();
+
+synth.triggerAttackRelease("C4", "16n");
+
+const canvas = document.querySelector('#root') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+const world = new World(Vec2(0, -10));
+const renderer = new Renderer(world, ctx);
+
+const runner = new Runner(world, {
+  // default settings
+  speed: 1,
+  fps: 60,
+});
+
+// init world entities
+world.createBody().createFixture(Edge(Vec2(-40.0, 0.0), Vec2(40.0, 0.0)));
+world.createDynamicBody(Vec2(0.0, 4.5)).createFixture(Circle(0.5), 10.0);
+world.createDynamicBody(Vec2(0.0, 10.0)).createFixture(Circle(5.0), 10.0);
+
+runner.start(() => {
+  renderer.renderWorld();
+});
